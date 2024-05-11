54c7ee4269056b13f8bf86719d490115e36f7ce0
TokeHOlesen <tokeholesen@gmail.com>
2024-05-11 10:36:26 +0200

    Type hints added.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import tkinter
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
@@ -6,103 +7,113 @@ from tkinter.messagebox import askyesno
 from datetime import datetime
 
 
-# Takes a BarTender database file and a similarly formatted corrections file as input and builds a list of known items
-# and their attributes. These attributes can then be retrieved by calling the associated method and the item's index in
-# the list. The index can be obtained by calling .get_index(barcode).
 class Cushion:
-    def __init__(self, bartender_path, corrections_path):
+    """
+    Takes a BarTender csv file and a similarly formatted corrections file as input and builds a list of known items
+    and their attributes. These attributes can then be retrieved by calling the associated method and the item's index
+    in the list. The index can be obtained by calling .get_index(barcode).
+    """
+    def __init__(self, bartender_file_path: str, corrections_file_path: str):
         bartender_entries = []
         try:
-            with open(bartender_path) as bartender_file:
+            with open(bartender_file_path) as bartender_file:
                 for line in bartender_file:
                     bartender_entries.append(line)
             bartender_entries.pop(0)
         except FileNotFoundError:
             messagebox.showerror("Fejl", "Kan ikke finde BarTender filen.")
             raise SystemExit
-        self.old_number = []
-        self.item_name = []
-        self.color = []
-        self.ean_13 = []
-        self.new_number = []
+        # Loads the data from the BarTender CSV file.
+        self.old_number: list[str] = []
+        self.item_name: list[str] = []
+        self.color: list[str] = []
+        self.ean_13: list[str] = []
+        self.new_number: list[str] = []
         for entry in bartender_entries:
             split_entry = entry.split(";")
-            self.old_number.append(split_entry[0][4:])
+            self.old_number.append(split_entry[0][4:])  # Trims the "005-" from the beginning of the number
             self.item_name.append(split_entry[1])
             self.color.append(split_entry[2])
             self.ean_13.append(split_entry[3])
-            self.new_number.append(split_entry[4][:-1])
+            self.new_number.append(split_entry[4][:-1])  # Trims the "\n" from the end of the line
 
+        # Loads the correction data from Rettelser.txt
         correction_entries = []
         try:
-            with open(corrections_path) as corrections_file:
+            with open(corrections_file_path) as corrections_file:
                 for line in corrections_file:
                     correction_entries.append(line)
             correction_entries.pop(0)
         except FileNotFoundError:
-            messagebox.showerror("Fejl", "Kan ikke finde filen Rettelser.txt.")
+            messagebox.showerror("Fejl", "Kan ikke finde filen \"Rettelser.txt\".")
             raise SystemExit
-        self.wrong_barcodes = []
-        self.corrected_barcodes = []
-        self.ask_for_color = []
-        self.alt_color = []
+        self.wrong_barcodes: list[str] = []
+        self.corrected_barcodes: list[str] = []
+        self.ask_for_color: list[bool] = []
+        self.alt_color: list[str] = []
         for entry in correction_entries:
             split_entry = entry.split(";")
             self.wrong_barcodes.append(split_entry[0])
             self.corrected_barcodes.append(split_entry[1])
             self.ask_for_color.append((bool(int(split_entry[2]))))
-            self.alt_color.append(split_entry[3][:-1])
+            self.alt_color.append(split_entry[3][:-1])  # Trims the "\n" from the end of the line
         
         self.barcode_has_been_entered = False
         self.correct_barcode = ""
 
-    # Returns the index of the barcode in question in .ean_13[]
-    # This index corresponds to indices in all other item property lists
-    def get_index_by_barcode(self, barcode):
-        for i, checked_barcode in enumerate(self.ean_13):
+    def get_index_by_barcode(self, barcode: str) -> int:
+        """
+        Returns the index of the barcode in question in self.ean_13.
+        This index corresponds to indices in all other item property lists
+        """
+        for index, checked_barcode in enumerate(self.ean_13):
             if checked_barcode == barcode:
-                return i
+                return index
 
-    def get_index_by_old_number(self, old_number):
-        for i, number in enumerate(self.old_number):
+    def get_index_by_old_number(self, old_number: str) -> int:
+        """
+        Returns the index of the barcode in question in self.old_number.
+        This index corresponds to indices in all other item property lists
+        """
+        for index, number in enumerate(self.old_number):
             if number == old_number:
-                return i
+                return index
 
-    def get_old_number(self, index):
+    def get_old_number(self, index: int) -> str:
         return self.old_number[index]
 
-    def get_item_name(self, index):
+    def get_item_name(self, index: int) -> str:
         return self.item_name[index]
 
-    def get_color(self, index):
+    def get_color(self, index: int) -> str:
         return self.color[index]
 
-    def get_ean_13(self, index):
+    def get_ean_13(self, index: int) -> str:
         return self.ean_13[index]
 
-    def get_new_number(self, index):
+    def get_new_number(self, index: int) -> str:
         return self.new_number[index]
 
-    def get_number_of_entries(self):
+    def get_number_of_entries(self) -> int:
         return len(self.item_name)
 
-    # Returns True if the barcode is listed in the BarTender database file
-    def barcode_is_valid(self, barcode):
-        for checked_barcode in self.ean_13:
-            if checked_barcode == barcode:
-                return True
+    def barcode_is_valid(self, barcode: str) -> bool:
+        """Returns True if the barcode is listed in the BarTender database file."""
+        if barcode in self.ean_13:
+            return True
         return False
 
-    # Returns True if the barcode is known to potentially be incorrect
-    def barcode_needs_correcting(self, barcode):
-        for checked_barcode in self.wrong_barcodes:
-            if checked_barcode == barcode:
-                return True
+    def barcode_needs_correcting(self, barcode: str) -> bool:
+        """Returns True if the barcode is known to potentially be incorrect."""
+        if barcode in self.wrong_barcodes:
+            return True
         return False
 
-    # Takes a potentially incorrect barcode as input and returns the correct one
-    # In case of doubt (a correct barcode on a potentially incorrect item) asks the user to confirm the color
-    def corrected_barcode(self, barcode):
+    def corrected_barcode(self, barcode: str) -> str:
+        """
+        Takes a potentially incorrect barcode as input and returns the correct one.
+        In case of doubt (a correct barcode on a potentially incorrect item) asks the user to confirm the color.
+        """
         for i, wrong_barcode in enumerate(self.wrong_barcodes):
             if wrong_barcode == barcode:
                 if self.ask_for_color[i]:
@@ -113,15 +124,15 @@ class Cushion:
                         return self.wrong_barcodes[i]
                 return self.corrected_barcodes[i]
     
-    def set_barcode_to_use(self, barcode):
+    def set_barcode_to_use(self, barcode: str) -> None:
         self.correct_barcode = barcode
 
-    def get_barcode_to_use(self):
+    def get_barcode_to_use(self) -> str:
         return self.correct_barcode
 
 
-# Reads the entered barcode, checks if it's correct, corrects it if it's not, and displays the item data
-def enter_barcode(barcode):
+def enter_barcode(barcode: str):
+    """Reads the entered barcode, checks if it's correct, corrects it if it's not, and displays the item data."""
     uncorrected_barcode = ""
     if cushions.barcode_needs_correcting(barcode):
         uncorrected_barcode = barcode
@@ -151,7 +162,7 @@ def enter_barcode(barcode):
 
 # If the barcode is valid sends a specified number of print jobs to the default printer, writes job details to the log
 # file and resets the window
-def print_barcode():
+def print_barcode() -> None:
     if not cushions.barcode_has_been_entered:
         enter_barcode(scan_entry_box.get())
     if not number_entry_box.get().isnumeric() or int(number_entry_box.get()) < 1 or int(number_entry_box.get()) > 105:
@@ -181,7 +192,7 @@ def print_barcode():
 
 
 # Prints the item chosen from the dropdown menu in manual mode
-def print_manual():
+def print_manual() -> None:
     old_number = cushions_drop_down_list.get().split(" ")[0]
     barcode = cushions.get_ean_13(cushions.get_index_by_old_number(old_number))
 
@@ -201,7 +212,7 @@ def print_manual():
 
 
 # Enters current job details into the log file
-def write_to_log_file(barcode):
+def write_to_log_file(barcode: str) -> None:
     current_tab = tab_control.tab(tab_control.select(), "text")
     time_and_date = str(datetime.now())[:-7]
     log_file_line = ""
@@ -227,7 +238,7 @@ def write_to_log_file(barcode):
 
 
 # Runs when changing tabs - if the currect tab is Scanner, sets focus to scan_entry_box
-def tab_changed(event):
+def tab_changed(event: tkinter.Event) -> None:
     tab = event.widget.tab('current')['text']
     if tab == "Scanner":
         scan_entry_box.focus_set()
@@ -278,7 +289,7 @@ tab_control.bind("<<NotebookTabChanged>>", tab_changed)
 
 # "Scanner" Tab
 
-    # Main elements
+# Main elements
 scan_prompt_label = Label(scanner_tab, text="Scan en vare:", font=("Segoe UI", "24"))
 
 scan_entry_box = Entry(scanner_tab, width=14, font=("Segoe UI", "32"))
@@ -296,7 +307,7 @@ number_entry_box.grid(row=0, column=1)
 
 print_button = Button(scanner_tab, text="Print", command=print_barcode, width=12, font=("Segoe UI", "16"))
 
-    # Item data display
+# Item data display
 item_name_label_frame = Frame(scanner_tab)
 item_name_label = Label(item_name_label_frame, text="Varenavn:", anchor="w", width=16)
 item_name_label_text = Label(item_name_label_frame, anchor="w", width=48)
@@ -329,7 +340,7 @@ ean13_label_text.grid(row=0, column=1)
 
 # "Manuel" Tab
 
-    # Main elements
+# Main elements
 
 choose_cushion_label = Label(manual_tab, text="Vælg type:", font=("Segoe UI", "24"))
 

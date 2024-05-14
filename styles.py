5ac5b0aa9899391a86f8c0996e2463e160b1063e
TokeHOlesen <tokeholesen@gmail.com>
2024-05-14 19:30:27 +0200

    Style sheet added.

---
+++ styles.py
@@ -0,0 +1,36 @@
+style_sheet = """
+        QLineEdit {
+            border: 1px solid #AAAAAA;
+            border-radius: 4px;
+            padding: 2px;
+            background: white;
+        }
+        QPushButton {
+            background-color: #F4F4F4;
+            border: 1px solid #AAAAAA;
+            border-radius: 4px;
+        }
+        QPushButton:hover {
+            background-color: #FFFFFF;
+        }
+        QPushButton:focus {
+            border: 2px solid #AAAAAA;
+        }
+        QSpinBox {
+            border: 1px solid #AAAAAA;
+            border-radius: 4px;
+        }
+        QSpinBox:up-button {
+            subcontrol-origin: content;
+            subcontrol-position: top right;
+            width: 20px;
+        }
+        QSpinBox:down-button {
+            subcontrol-origin: content;
+            subcontrol-position: bottom right;
+            width: 20px;
+        }
+        QScrollBar {
+            border: 0px;
+        }
+    """

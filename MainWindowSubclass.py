dad3e6900fcfc1ec2e1ef4e111f932f8b8cc2e47
TokeHOlesen <tokeholesen@gmail.com>
2024-05-20 20:57:54 +0200

    MainWindowSubclass.py refactoring.

--- MainWindowSubclass.py
+++ MainWindowSubclass.py
@@ -20,42 +20,72 @@ class MainWindow(QMainWindow):
         self.sizes = sizes
         self.printers = printers
         self.item_data = item_data
-        # Sets window properties.
-        self.setWindowTitle("Hyndescanner")
-        self.setWindowIcon(QIcon(".\\Data\\barcode-scan.ico"))
-        self.setFixedSize(*sizes.main_window)
-        # File menu.
-        menu = self.menuBar()
-        file_menu = menu.addMenu("&Fil")
-        # Creates a "Choose printer" submenu and adds it to the File menu.
-        printer_submenu = file_menu.addMenu("Vælg &printer")
-        # Creates a "Default printer" menu item and its associated action. When selected, it will set the Windows
-        # default printer as the printer to use, and set the corresponding menu items as checked.
+        self.set_window_properties()
+        self.scanner_tab = ScannerTab(fonts, sizes, item_data, printers)
+        self.manuel_tab = ManualTab(fonts, sizes, item_data, printers)
+        self.setup_tabbed_interface()
+        # Creates a "Default printer" menu item and its associated action.
+        # When selected, it will set the Windows default printer as the printer to use.
         self.default_printer_action = QAction("Windows Standardprinter", self, checkable=True)
         self.default_printer_action.triggered.connect(self.use_default_printer)
+        # A QActionGroup for QActions associated with all printer devices.
+        self.printer_group = QActionGroup(self)
+        # Runs the menu setup methods.
+        menu = self.menuBar()
+        self.setup_file_menu(menu)
+        self.setup_edit_menu(menu)
+        self.setup_help_menu(menu)
+
+    def set_window_properties(self) -> None:
+        """Sets the window name, icon and size, and sets its position to the center of the screen."""
+        self.setWindowTitle("Hyndescanner")
+        self.setWindowIcon(QIcon(".\\Data\\barcode-scan.ico"))
+        self.setFixedSize(*self.sizes.main_window)
+        screen = QGuiApplication.primaryScreen().geometry()
+        center_pos_x = (screen.width() - self.width()) // 2
+        center_pos_y = (screen.height() - self.height()) // 2
+        self.move(center_pos_x, center_pos_y)
+
+    def setup_file_menu(self, menu) -> None:
+        """
+        Adds the File menu elements - a 'Choose printer' submenu and an Exit command.
+        The 'Choose printer' submenu gets populated with all printers available on the system.
+        If the user previously selected a printer, it will be set as selected; otherwise, the Windows default printer
+        will be used.
+        Puts a checkmark next to the selected printer's name.
+        """
+        file_menu = menu.addMenu("&Fil")
+        printer_submenu = file_menu.addMenu("Vælg &printer")
         printer_submenu.addAction(self.default_printer_action)
         printer_submenu.addSeparator()
-        # Creates a QActionGroup for all printer devices; adds their corresponding menu items to the printer submenu.
-        self.printer_group = QActionGroup(self)
-        for printer_name in printers.available_printer_names:
-            action = QAction(printer_name, self, checkable=True)
-            action.triggered.connect(lambda _, printer=action.text(): self.select_printer(printer))
-            printer_submenu.addAction(action)
-            self.printer_group.addAction(action)
-        # Sets the printer submenu items' checked status according to the currently selected printer.
-        for printer_action in self.printer_group.actions():
-            if printer_action.text() == printers.selected_printer_name:
-                printer_action.setChecked(True)
-                self.default_printer_action.setChecked(printer_action.text() == printers.default_printer_name)
-                break
-        else:
-            self.default_printer_action.setChecked(True)
+        self.add_printers_to_menu(printer_submenu)
+        self.set_printer_menu_items_checked_status()
         file_menu.addSeparator()
         exit_action = QAction("&Afslut", self)
         exit_action.triggered.connect(sys.exit)
         exit_action.setShortcut("Ctrl+Q")
         file_menu.addAction(exit_action)
-        # Edit menu
+
+    def add_printers_to_menu(self, printer_submenu) -> None:
+        """Loops through available printers, creates actions for them and adds them to the "Choose printer" submenu."""
+        for printer_name in self.printers.available_printer_names:
+            action = QAction(printer_name, self, checkable=True)
+            action.triggered.connect(lambda _, printer=action.text(): self.select_printer(printer))
+            printer_submenu.addAction(action)
+            self.printer_group.addAction(action)
+
+    def set_printer_menu_items_checked_status(self) -> None:
+        """Sets the printer submenu items' checked status according to the currently selected printer."""
+        for printer_action in self.printer_group.actions():
+            if printer_action.text() == self.printers.selected_printer_name:
+                printer_action.setChecked(True)
+                self.default_printer_action.setChecked(printer_action.text() == self.printers.default_printer_name)
+                break
+        else:
+            self.default_printer_action.setChecked(True)
+
+    def setup_edit_menu(self, menu) -> None:
+        """Sets up an Edit menu, letting the user open the associated text files."""
         edit_menu = menu.addMenu("&Rediger")
         open_database_action = QAction("Vis &BarTender CSV-filen", self)
         open_corrections_action = QAction("Vis listen over rettelser", self)
@@ -67,27 +97,21 @@ class MainWindow(QMainWindow):
         open_database_action.triggered.connect(self.open_bartender_file)
         open_corrections_action.triggered.connect(self.open_corrections_file)
         open_logfile_action.triggered.connect(self.open_log_file)
-        # Help menu
+
+    def setup_help_menu(self, menu) -> None:
+        """Sets up the Help menu."""
         help_menu = menu.addMenu("&Hjælp")
         about_action = QAction("&Om...", self)
         about_action.triggered.connect(self.open_about_window)
         help_menu.addAction(about_action)
 
-        # Moves the window to the center of the screen
-        screen = QGuiApplication.primaryScreen().geometry()
-        center_pos_x = (screen.width() - self.width()) // 2
-        center_pos_y = (screen.height() - self.height()) // 2
-        self.move(center_pos_x, center_pos_y)
-        # Instantiates the main widgets
-        self.scanner_tab = ScannerTab(fonts, sizes, item_data, printers)
-        self.manuel_tab = ManualTab(fonts, sizes, item_data, printers)
-        # Instantiates the tabs
+    def setup_tabbed_interface(self) -> None:
+        """Creates a QTabWidget, adds the two tab-widgets to it and sets it as the window's central widget."""
         tab_widget = QTabWidget()
         tab_widget.setTabPosition(QTabWidget.TabPosition.North)
         tab_widget.setMovable(False)
         tab_widget.addTab(self.scanner_tab, "Scanner")
         tab_widget.addTab(self.manuel_tab, "Manuel")
-        # Sets the central widget
         self.setCentralWidget(tab_widget)
 
     def use_default_printer(self) -> None:
